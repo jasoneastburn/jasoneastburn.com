4a3ae617d122759b98ab2340e4295199f68a5dc7
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2022-06-29 19:35:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'testsite',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

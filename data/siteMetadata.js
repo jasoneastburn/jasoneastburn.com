4d8a489fa4bef56ffce4bbd3a78f50db651b4e81
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2024-03-18 13:56:31 -0500

    add favicons

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'jasoneastburn.com',
+  title: 'Jason Eastburn',
   author: 'Jason Eastburn',
   headerTitle: 'Jason Eastburn',
   description: 'A blog created with Next.js and Tailwind.css',

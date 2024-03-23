bdd4da8be4a73a7887471d1f5b415fe4ab78ffe3
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2024-03-22 23:55:03 -0500

    remove console log

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   const user = await signIn()
-  console.log(user)
   return (
     <div>
       <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">

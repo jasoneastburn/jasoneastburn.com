1844db55b097e97d89f02c99372c8edb4db63955
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2025-02-24 22:24:10 -0600

    update footer layout

--- components/footer/BuiltWith.tsx
+++ components/footer/BuiltWith.tsx
@@ -4,7 +4,6 @@ import siteMetadata from '@/data/siteMetadata'
 
 const BuiltWith = () => (
   <div className="flex items-center space-x-1">
-    <span className="mr-1 text-gray-500 dark:text-gray-400">Built with</span>
     <div className="flex space-x-1.5">
       <Link href="https://nextjs.org">
         <BrandIcon type="NextJS" className="h-5 w-5" />
@@ -14,6 +13,9 @@ const BuiltWith = () => (
       </Link>
       <Link href="https://www.typescriptlang.org">
         <BrandIcon type="Typescript" className="h-5 w-5" />
+      </Link>
+      <Link href="https://www.markdownguide.org">
+        <BrandIcon type="Markdown" className="h-5 w-5" />
       </Link>
       <Link href={siteMetadata.siteRepo}>
         <BrandIcon type="GitHub" className="h-5 w-5" />
--- components/ui/BrandIcon.tsx
+++ components/ui/BrandIcon.tsx
@@ -1,64 +1,60 @@
-import Bash from 'public/static/icons/bash.svg'
-import Git from 'public/static/icons/git.svg'
-import GitHub from 'public/static/icons/github.svg'
-import Javascript from 'public/static/icons/javascript.svg'
-import Liquid from 'public/static/icons/liquid.svg'
-import Markdown from 'public/static/icons/markdown.svg'
-import NextJS from 'public/static/icons/nextjs.svg'
-import Node from 'public/static/icons/nodejs.svg'
-import Prisma from 'public/static/icons/prisma.svg'
-import Railway from 'public/static/icons/railway.svg'
-import React from 'public/static/icons/react.svg'
-import Remix from 'public/static/icons/remix.svg'
-import Spotify from 'public/static/icons/spotify.svg'
-import TailwindCSS from 'public/static/icons/tailwind.svg'
-import Typescript from 'public/static/icons/typescript.svg'
-import Umami from 'public/static/icons/umami.svg'
-import Vercel from 'public/static/icons/vercel.svg'
-import NestJS from 'public/static/icons/nestjs.svg'
-import Docker from 'public/static/icons/docker.svg'
-import Postgres from 'public/static/icons/postgres.svg'
-import Mongodb from 'public/static/icons/mongodb.svg'
+import Bash from 'public/icons/bash.svg'
+import Docker from 'public/icons/docker.svg'
+import Git from 'public/icons/git.svg'
+import GitHub from 'public/icons/github.svg'
+import Javascript from 'public/icons/javascript.svg'
+import Liquid from 'public/icons/liquid.svg'
+import Markdown from 'public/icons/markdown.svg'
+import Mongodb from 'public/icons/mongodb.svg'
+import NestJS from 'public/icons/nestjs.svg'
+import NextJS from 'public/icons/nextjs.svg'
+import Node from 'public/icons/nodejs.svg'
+import Postgres from 'public/icons/postgres.svg'
+import Prisma from 'public/icons/prisma.svg'
+import Railway from 'public/icons/railway.svg'
+import React from 'public/icons/react.svg'
+import Remix from 'public/icons/remix.svg'
+import Spotify from 'public/icons/spotify.svg'
+import TailwindCSS from 'public/icons/tailwind.svg'
+import Typescript from 'public/icons/typescript.svg'
+import Umami from 'public/icons/umami.svg'
+import Vercel from 'public/icons/vercel.svg'
 
 export const BrandIconsMap = {
-  React,
-  Remix,
+  Bash,
+  Docker,
   Git,
   GitHub,
   Javascript,
-  Typescript,
-  Node,
-  Bash,
   Liquid,
   Markdown,
+  Mongodb,
+  NestJS,
   NextJS,
-  TailwindCSS,
+  Node,
+  Postgres,
   Prisma,
+  Railway,
+  React,
+  Remix,
+  Spotify,
+  TailwindCSS,
+  Typescript,
   Umami,
   Vercel,
-  Railway,
-  Spotify,
-  NestJS,
-  Docker,
-  Postgres,
-  Mongodb,
 }
 
 export type BrandIconType = keyof typeof BrandIconsMap
 
 const BrandIcon = (props: { type: keyof typeof BrandIconsMap; className?: string }) => {
-  const { type, className } = props
-
+  const { className, type } = props
   const Icon = BrandIconsMap[type]
-
-  if (!Icon) {
-    return <div>Missing icon for {type}.</div>
-  }
 
   return (
     <Icon
       className={className || 'h-16 w-16 lg:h-14 lg:w-14 xl:h-20 xl:w-20'}
       fill="currentColor"
+      alt={`Icon for ${type}`}
     />
   )
 }

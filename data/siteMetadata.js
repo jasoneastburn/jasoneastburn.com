278e33db27b15ebaeb184d2f14e9237a1ccb9d52
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2025-02-24 20:40:07 -0600

    enable post comments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -15,7 +15,6 @@ const siteMetadata = {
   email: 'jasoneastburn@gmail.com',
   github: 'https://github.com/jasoneastburn',
   x: 'https://x.com/jasoneastburn',
-  // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com/jasoneastburn',
   youtube: 'https://youtube.com/channel/UCK9bWbp8I-exu4cD4qFTs8A',
   linkedin: 'https://www.linkedin.com/in/jasoneastburn',
@@ -24,7 +23,6 @@ const siteMetadata = {
   medium: '',
   bluesky: 'https://bsky.app/profile/jasoneastburn.bsky.social',
   locale: 'en-US',
-  // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -47,7 +45,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'G-V815YKDS4L', // e.g. G-XXXXXXX
+      googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. G-XXXXXXX
     },
   },
   newsletter: {
@@ -56,34 +54,21 @@ const siteMetadata = {
     provider: 'buttondown',
   },
   comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    //provider: 'giscus', // supported providers: giscus, utterances, disqus
-    //giscusConfig: {
-    // Visit the link below, and follow the steps in the 'configuration' section
-    // https://giscus.app/
-    //repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-    //repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-    //category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-    //categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-    //mapping: 'pathname', // supported options: pathname, url, title
-    //reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-    // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-    //metadata: '0',
-    // theme example: light, dark, dark_dimmed, dark_high_contrast
-    // transparent_dark, preferred_color_scheme, custom
-    //theme: 'light',
-    // theme when dark mode
-    //darkTheme: 'transparent_dark',
-    // If the theme option above is set to 'custom`
-    // please provide a link below to your custom theme css file.
-    // example: https://giscus.app/themes/custom_example.css
-    //themeURL: '',
-    // This corresponds to the `data-lang="en"` in giscus's configurations
-    //lang: 'en',
-    //},
+    provider: 'giscus',
+    giscusConfig: {
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      mapping: 'pathname',
+      reactions: '1',
+      metadata: '0',
+      theme: 'transparent_dark',
+      darkTheme: 'transparent_dark',
+      themeURL: '',
+      lang: 'en',
+      inputPosition: 'top',
+    },
   },
   search: {
     provider: 'kbar', // kbar or algolia

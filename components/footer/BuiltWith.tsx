c36776d5cc946fdd2de938c6f36f9f1624f9d038
Jason Eastburn <jasoneastburn@gmail.com>
2025-02-24 21:10:43 -0600

    update footer

--- components/footer/BuiltWith.tsx
+++ components/footer/BuiltWith.tsx
@@ -1,12 +1,10 @@
-import siteMetadata from '@/data/siteMetadata'
-
-import Link from '@/components/ui/Link'
 import BrandIcon from '@/components/ui/BrandIcon'
+import Link from '@/components/ui/Link'
+import siteMetadata from '@/data/siteMetadata'
 
 const BuiltWith = () => (
   <div className="flex items-center space-x-1">
     <span className="mr-1 text-gray-500 dark:text-gray-400">Built with</span>
-
     <div className="flex space-x-1.5">
       <Link href="https://nextjs.org">
         <BrandIcon type="NextJS" className="h-5 w-5" />
@@ -17,14 +15,10 @@ const BuiltWith = () => (
       <Link href="https://www.typescriptlang.org">
         <BrandIcon type="Typescript" className="h-5 w-5" />
       </Link>
+      <Link href={siteMetadata.siteRepo}>
+        <BrandIcon type="GitHub" className="h-5 w-5" />
+      </Link>
     </div>
-    <span className="px-1 text-gray-400 dark:text-gray-500">-</span>
-    <Link
-      href={siteMetadata.siteRepo}
-      className="text-gray-500 underline underline-offset-4 dark:text-gray-400"
-    >
-      <span>View source</span>
-    </Link>
   </div>
 )
 

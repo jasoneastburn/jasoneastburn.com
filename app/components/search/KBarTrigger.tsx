690d82db98814be88b6259887dbca9f42d1582fb
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2025-03-07 22:38:29 -0600

    remove kbar for now

--- app/components/search/KBarTrigger.tsx
+++ app/components/search/KBarTrigger.tsx
@@ -1,17 +1,17 @@
-import { useKBar } from 'kbar'
-import { Command } from 'lucide-react'
+// import { useKBar } from 'kbar'
+// import { Command } from 'lucide-react'
 
-export function KbarSearchTrigger() {
-  const { query } = useKBar()
+// export function KbarSearchTrigger() {
+//   const { query } = useKBar()
 
-  return (
-    <button
-      aria-label="Search"
-      className="rounded p-1.5 hover:bg-gray-200 dark:hover:bg-gray-700"
-      data-umami-event="search-the-site"
-      onClick={() => query.toggle()}
-    >
-      <Command size={20} strokeWidth={1.5} />
-    </button>
-  )
-}
+//   return (
+//     <button
+//       aria-label="Search"
+//       className="rounded p-1.5 hover:bg-gray-200 dark:hover:bg-gray-700"
+//       data-umami-event="search-the-site"
+//       onClick={() => query.toggle()}
+//     >
+//       <Command size={20} strokeWidth={1.5} />
+//     </button>
+//   )
+// }

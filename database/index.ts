1abbdec068e20813cc88050ac3b70cfbcbb8d315
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2025-03-08 10:31:54 -0600

    fix db connection on build

--- database/index.ts
+++ database/index.ts
@@ -1,14 +1,12 @@
-import 'dotenv/config'
-
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import '../envConfig'
+import { config } from 'dotenv'
 
-const connectionString = process.env.DATABASE_URL
+config({ path: '.env' })
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not defined')
-}
+export const client = postgres(process.env.DATABASE_URL!)
+export const db = drizzle({ client })
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false })
-export const db = drizzle(client)
+async function main() {}
+main()

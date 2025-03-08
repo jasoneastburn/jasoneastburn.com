2186a82624cd077f994909edf0c8d9f123cc2190
Jason Eastburn <jasoneastburn@gmail.com>
2025-03-07 23:44:57 -0600

    fixed css for icons

--- app/components/ui/Timeline.tsx
+++ app/components/ui/Timeline.tsx
@@ -44,9 +44,9 @@ function TimelineItem({ exp, last }: { exp: CareerTimeline; last: boolean }) {
         width={200}
         height={200}
       />
-      <details className="w-full [&_.minus]:open:block [&_.plus]:open:hidden">
+      <details className={clsx('w-full [&[open]_.minus]:visible [&[open]_.plus]:hidden')}>
         <summary className="relative pr-10 marker:content-none">
-          {/* <Plus
+          <Plus
             size={18}
             className={clsx([
               'plus',
@@ -55,18 +55,17 @@ function TimelineItem({ exp, last }: { exp: CareerTimeline; last: boolean }) {
               'transition-transform duration-300 ease-in-out',
               'text-gray-600 dark:text-gray-500',
             ])}
-            data-umami-event={`${event}-expand`}
           />
           <Minus
             size={18}
             className={clsx([
-              'minus hidden',
+              'minus',
+              'group-hover/timeline-item:visible md:invisible',
               'absolute top-1 right-1',
               'transition-transform duration-300 ease-in-out',
               'text-gray-600 dark:text-gray-500',
             ])}
-            data-umami-event={`${event}-collapse`}
-          /> */}
+          />
           <div className="flex flex-col">
             <div className="line-clamp-1 text-xs text-gray-500 tabular-nums dark:text-gray-400">
               <span>{start}</span> – <span>{end}</span>

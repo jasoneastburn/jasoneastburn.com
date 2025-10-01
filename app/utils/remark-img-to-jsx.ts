9bbec93f77ff9b55de41225e518b2f59c4fcf880
Jason Eastburn <jasoneastburn@users.noreply.github.com>
2025-09-30 21:28:05 -0500

    fix: lint error

--- app/utils/remark-img-to-jsx.ts
+++ app/utils/remark-img-to-jsx.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
 import fs from 'fs'
 import { sync as sizeOf } from 'probe-image-size'
 import type { Parent, Node, Literal } from 'unist'
@@ -24,14 +23,14 @@ export function remarkImgToJsx() {
         if (fs.existsSync(`${process.cwd()}/public${imageNode.url}`)) {
           const dimensions = sizeOf(fs.readFileSync(`${process.cwd()}/public${imageNode.url}`))
 
-          ;(imageNode.type = 'mdxJsxFlowElement'),
-            (imageNode.name = 'Image'),
-            (imageNode.attributes = [
-              { type: 'mdxJsxAttribute', name: 'alt', value: imageNode.alt },
-              { type: 'mdxJsxAttribute', name: 'src', value: imageNode.url },
-              { type: 'mdxJsxAttribute', name: 'width', value: dimensions.width },
-              { type: 'mdxJsxAttribute', name: 'height', value: dimensions.height },
-            ])
+          imageNode.type = 'mdxJsxFlowElement'
+          imageNode.name = 'Image'
+          imageNode.attributes = [
+            { type: 'mdxJsxAttribute', name: 'alt', value: imageNode.alt },
+            { type: 'mdxJsxAttribute', name: 'src', value: imageNode.url },
+            { type: 'mdxJsxAttribute', name: 'width', value: dimensions.width },
+            { type: 'mdxJsxAttribute', name: 'height', value: dimensions.height },
+          ]
 
           node.type = 'div'
           node.children[imageNodeIndex] = imageNode
